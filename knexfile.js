6ab7f17220080bec2ee5437a36057fd6fb04ff8b
Pablo Alan <pabloxt14@gmail.com>
2023-01-04 17:18:06 -0300

    feat: created links migration and tags migration

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
+    pool: {
+      afterCreate: (connection, callback) => connection.run("PRAGMA foreign_keys = ON", callback)
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations"),
     },
---
+++ src/database/knex/migrations/20230104194903_createTags.js
@@ -0,0 +1,9 @@
+exports.up = knex => knex.schema.createTable("tags", table => {
+  table.increments("id");
+  table.text("name");
+
+  table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");// quando deletar uma <note> específica deleta também as <tags> relacionadas a está <note>
+  table.integer("user_id").references("id").inTable("users");
+});
+
+exports.down = knex => knex.schema.dropTable("tags");
