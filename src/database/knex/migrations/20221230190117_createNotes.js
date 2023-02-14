29f29cad9f78ecd31eb24154e8fe6362976d83aa
Pablo Alan <pabloxt14@gmail.com>
2023-02-14 16:46:49 -0300

    chore: removing <;> from TagsController

--- src/database/knex/migrations/20221230190117_createNotes.js
+++ src/database/knex/migrations/20221230190117_createNotes.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("notes", table => {
   table.increments("id");
   table.text("title");
   table.text("description");
-  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");;
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
--- src/database/knex/migrations/20230104194903_createTags.js
+++ src/database/knex/migrations/20230104194903_createTags.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("tags", table => {
   table.text("name");
 
   table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");// quando deletar uma <note> específica deleta também as <tags> relacionadas a está <note>
-  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");;
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
 });
 
 exports.down = knex => knex.schema.dropTable("tags");
